13d296842919ebff253c2e207bc4fd7890d72470
c062dsy0739 <128620776+c062dsy0739@users.noreply.github.com>
2023-05-15 15:42:23 +0000

    change config port in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 // const bodyParser = require('body-parser');
-const {PORT} = process.env
+process.env.PORT = 7000
 app.listen(PORT, ()=>{
   console.log(`Server is running on port: ${PORT}`)
 })
