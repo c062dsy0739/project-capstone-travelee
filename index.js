a81ccb76261213c5a20e331d2407c0680e88a0e7
c062dsy0739 <128620776+c062dsy0739@users.noreply.github.com>
2023-05-15 15:54:35 +0000

    modified:   .devcontainer/devcontainer.json 	modified:   index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 // const bodyParser = require('body-parser');
-const {port} = process.env.PORT || 7000
+const port = process.env.PORT || 6000;
 app.listen(port, ()=>{
   console.log(`Server is running on port: ${port}`)
 })
