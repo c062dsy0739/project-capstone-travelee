0636414ff15d53e9615caf79485eea05bd8b36bb
c062dsy0739 <128620776+c062dsy0739@users.noreply.github.com>
2023-05-15 15:32:30 +0000

    fixed index configuration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app')
 // const bodyParser = require('body-parser');
 const {PORT} = process.env
 app.listen(PORT, ()=>{
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port: ${PORT}`)
 })
 
 
