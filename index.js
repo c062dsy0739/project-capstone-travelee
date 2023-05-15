7292a4787899d234a8e6ecaf13758acfccf1603c
c062dsy0739 <128620776+c062dsy0739@users.noreply.github.com>
2023-05-15 17:17:20 +0000

    change index env

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const app = require('./app')
-// const bodyParser = require('body-parser');
 const port = process.env.PORT || 6000;
 app.listen(port, ()=>{
   console.log(`Server is running on port: ${port}`)
