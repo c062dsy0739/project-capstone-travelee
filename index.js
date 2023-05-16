d0658399e873094a99f7d635b45fa3495a886192
c062dsy2377 <133129602+c062dsy2377@users.noreply.github.com>
2023-05-16 08:32:07 +0000

    update port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7000;
 app.listen(port, ()=>{
   console.log(`Server is running on port: ${port}`)
 })
