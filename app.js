1542675bec6af59cb7fa78610af009d722853ba4
c062dsy2377 <133129602+c062dsy2377@users.noreply.github.com>
2023-05-21 13:53:41 +0000

    ngubah dr otp ke link

--- app.js
+++ app.js
@@ -28,40 +28,79 @@ app.post("/register", async (req, res) => {
       password
     });
 
-    // Kirim kode OTP ke email user
-    const otpCode = generateOTP(); // Fungsi untuk menghasilkan kode OTP
-    await sendEmail(email, otpCode);
+    // Kirim link verifikasi ke email pengguna
+    const verificationToken = generateVerificationToken(); // Fungsi untuk menghasilkan token verifikasi
+    const verificationLink = `${process.env.BASE_URL}/verify/${verificationToken}`;
+    // Kirim email verifikasi dengan link ke pengguna
+    await sendVerificationEmail(email, verificationLink);
     res.status(200).json({ message: "User registered successfully." });
+  //   // Kirim kode OTP ke email user
+  //   const otpCode = generateOTP(); // Fungsi untuk menghasilkan kode OTP
+  //   await sendEmail(email, otpCode);
+  //   res.status(200).json({ message: "User registered successfully." });
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ error: "Failed to register user." });
   }
 });
 
-//Verify code OTP 
+
+// Verify code Link
 app.post('/verify', async (req, res) => {
   try {
-    const { email, otpCode } = req.body;
+    const { token } = req.body;
 
-    // Ambil kode OTP dari Firestore
-    const otpDoc = await db.collection('otp').doc(email).get();
-    if (!otpDoc.exists) {
-      return res.status(400).json({ error: 'Invalid email or OTP code.' });
+    // Cari pengguna dengan token verifikasi yang cocok
+    const userQuery = await db
+      .collection('users')
+      .where('verificationToken', '==', token)
+      .limit(1)
+      .get();
+
+    if (userQuery.empty) {
+      return res.status(400).json({ error: 'Invalid verification token.' });
     }
 
-    const storedOtpCode = otpDoc.data().code;
-    if (otpCode !== storedOtpCode) {
-      return res.status(400).json({ error: 'Invalid email or OTP code.' });
-    }
+    const user = userQuery.docs[0];
+    const userId = user.id;
 
-    // Hapus kode OTP dari Firestore setelah verifikasi berhasil
-    await db.collection('otp').doc(email).delete();
+    // Verifikasi akun dan hapus token verifikasi
+    await db.collection('users').doc(userId).update({
+      verified: true,
+      verificationToken: null
+    });
 
-    res.status(200).json({ message: 'Verification successful.' });
+    res.status(200).json({ message: 'Account verification successful.' });
   } catch (error) {
-    console.error('Error verifying OTP:', error);
-    res.status(500).json({ error: 'Failed to verify OTP.' });
+    console.error('Error verifying account:', error);
+    res.status(500).json({ error: 'Failed to verify account.' });
   }
 });
+
+// //Verify code OTP 
+// app.post('/verify', async (req, res) => {
+//   try {
+//     const { email, otpCode } = req.body;
+
+//     // Ambil kode OTP dari Firestore
+//     const otpDoc = await db.collection('otp').doc(email).get();
+//     if (!otpDoc.exists) {
+//       return res.status(400).json({ error: 'Invalid email or OTP code.' });
+//     }
+
+//     const storedOtpCode = otpDoc.data().code;
+//     if (otpCode !== storedOtpCode) {
+//       return res.status(400).json({ error: 'Invalid email or OTP code.' });
+//     }
+
+//     // Hapus kode OTP dari Firestore setelah verifikasi berhasil
+//     await db.collection('otp').doc(email).delete();
+
+//     res.status(200).json({ message: 'Verification successful.' });
+//   } catch (error) {
+//     console.error('Error verifying OTP:', error);
+//     res.status(500).json({ error: 'Failed to verify OTP.' });
+//   }
+// });
 
 module.exports = app;
