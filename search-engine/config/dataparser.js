a4ac5fc050fbcbc7824fa1eaa70fb9c371aad2cf
c062dsy2377 <133129602+c062dsy2377@users.noreply.github.com>
2023-06-05 23:04:42 +0700

    Update dataparser.js

--- search-engine/config/dataparser.js
+++ search-engine/config/dataparser.js
@@ -1,22 +1,16 @@
-const csv = require("csv-parser");
-const fs = require("fs");
+const connection = require('../config/database');
 
-const dataParser = () => {
+function selectQuery() {
   return new Promise((resolve, reject) => {
-    const results = [];
-    fs.createReadStream("eco-place.csv")
-      .pipe(csv())
-      .on("data", (data) => {
-        results.push(data);
-      })
-      .on("end", () => {
-        console.log("Proses membaca file CSV selesai");
-        resolve(results);
-      })
-      .on("error", (error) => {
+    const selectQuery = 'SELECT * FROM place_destination';
+    connection.query(selectQuery, (error, results) => {
+      if (error) {
         reject(error);
-      });
+      } else {
+        resolve(results);
+      }
+    });
   });
-};
+}
 
-module.exports = dataParser;
+module.exports = selectQuery;
