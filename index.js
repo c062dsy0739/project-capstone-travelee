12d95c986bd177caef5cc7cb43ecd747c126cdfe
c062dsy0739 <c062dsy0739@bangkit.academy>
2023-06-09 12:38:14 +0700

    modified:   app.yaml 	deleted:    collaborative-content/README.md 	modified:   index.js 	deleted:    package-lock.json 	deleted:    package.json

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 // Import app.js dari setiap folder
-// Import app.js dari setiap folder
 const app1 = require('./authentication/app');
 const app2 = require('./search-engine/app');
 const app3 = require('./favorite-place/app');
@@ -9,7 +8,7 @@ const app4 = require('./event/app');
 //const app5 = require('./folder5/app');
 const index = express();
 
-index.use(app1);
-index.use(app2);
-index.use(app3);
-index.use(app4);
+index.use('/authentication',app1);
+index.use('/seacrh-engine',app2);
+index.use('/favorite-place',app3);
+index.use('/event',app4);
