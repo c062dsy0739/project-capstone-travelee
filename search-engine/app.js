91721ea98d22b21dc2c7a04e8ae4c94275dbc33b
c062dsy2377 <133129602+c062dsy2377@users.noreply.github.com>
2023-06-10 17:30:41 +0700

    Update app2.js

--- search-engine/app.js
+++ search-engine/app.js
@@ -2,11 +2,31 @@ const express = require("express");
 const app2 = express();
 const connection = require("./config/database");
 const selectQuery = require("./config/dataparser");
+const jwt = require("jsonwebtoken");
 
 app2.use(express.json());
 
+// Middleware untuk verifikasi token JWT
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not provided' });
+  }
+
+  jwt.verify(token, 'your-secret-key', (err, payload) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    // Tambahkan payload token ke objek request untuk digunakan di endpoint berikutnya
+    req.user = payload;
+    next();
+  });
+}
+
 // Mengatur endpoint untuk mengambil semua data
-app2.get("/place", (req, res) => {
+app2.get("/place", verifyToken, (req, res) => {
   selectQuery()
     .then((results) => {
       res.json({
@@ -24,7 +44,7 @@ app2.get("/place", (req, res) => {
 });
 
 // Mengatur endpoint untuk mengambil data berdasarkan place_id
-app2.get("/place/:id", (req, res) => {
+app2.get("/place/:id", verifyToken, (req, res) => {
   const placeId = req.params.id;
 
   // Melakukan query untuk mendapatkan data berdasarkan place_id
@@ -55,7 +75,7 @@ app2.get("/place/:id", (req, res) => {
 });
 
 // Mengatur endpoint untuk mencari data berdasarkan place_name
-app2.get("/place/search/name", (req, res) => {
+app2.get("/place/search/name", verifyToken, (req, res) => {
   const placeName = req.query.name;
 
   // Melakukan query untuk mencari data berdasarkan place_name
@@ -80,7 +100,7 @@ app2.get("/place/search/name", (req, res) => {
 });
 
 // Mengatur endpoint untuk mencari data berdasarkan city
-app2.get("/place/search/city", (req, res) => {
+app2.get("/place/search/city", verifyToken, (req, res) => {
   const city = req.query.city;
 
   // Melakukan query untuk mencari data berdasarkan city
@@ -104,29 +124,29 @@ app2.get("/place/search/city", (req, res) => {
   );
 });
 
-// Mengatur endpoint untuk mencari data berdasarkan city
-app2.get("/place/search/category", (req, res) => {
-    const category = req.query.category;
-  
-    // Melakukan query untuk mencari data berdasarkan city
-    connection.query(
-      "SELECT * FROM place_destination WHERE category LIKE ?",
-      [`%${category}%`],
-      (error, results) => {
-        if (error) {
-          console.error(error);
-          res.status(500).json({
-            status: "error",
-            message: "Internal server error",
-          });
-        }
-  
-        res.json({
-          status: "success",
-          data: results,
+// Mengatur endpoint untuk mencari data berdasarkan category
+app2.get("/place/search/category", verifyToken, (req, res) => {
+  const category = req.query.category;
+
+  // Melakukan query untuk mencari data berdasarkan category
+  connection.query(
+    "SELECT * FROM place_destination WHERE category LIKE ?",
+    [`%${category}%`],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({
+          status: "error",
+          message: "Internal server error",
         });
       }
-    );
-  });
+
+      res.json({
+        status: "success",
+        data: results,
+      });
+    }
+  );
+});
 
 module.exports = app2;
