eebeae8ecbef7b6c4efc518082532d4aff0ee9db
c062dsy0739 <128620776+c062dsy0739@users.noreply.github.com>
2023-06-12 20:52:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app1 = require('./authentication/app');
 const app2 = require('./search-engine/app');
 const app3 = require('./favorite-place/app');
 const app4 = require('./event/app');
-//const app5 = require('./folder5/app');
+
 const index = express();
 
 index.use('/authentication', app1);
@@ -14,7 +14,7 @@ index.use('/favorite-place', app3);
 index.use('/event', app4);
 
 // Menambahkan konfigurasi server
-const PORT = process.env.PORT || 8080;
+const PORT =  process.env.PORT || 7000;
 index.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
